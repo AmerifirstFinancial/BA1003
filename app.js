170bc92397e9fffa16c740afbc81c07a7a15c58a
bwchronos <Chipper1608>
2016-07-01 17:38:26 -0700

    added layout

--- app.js
+++ app.js
@@ -3,23 +3,26 @@ var app = express();
 
 app.set('view engine','ejs');
 
+//app.set('views',__dirname+'/views');
+
 app.get('/',function(req,res){
-   res.render('default');
-});
-app.get('/me',function(req,res){
-    res.send('helllo, me');
+    res.render('default',
+        {
+            title: 'Home',
+            classname: 'home'
+        }
+    );
 });
 
-app.get('/who/:name?',function(req,res){
-    var name = req.params.name;
-    res.send('Hello, '+name);
+app.get('/about', function (req, res) {
+    res.render('default',
+        {
+            title: 'About',
+            classname: 'about'
+        }
+    );
 });
-app.get('/who/:name?/:title?',function(req,res){
-    var name = req.params.name;
-    var title = req.params.title;
-    res.send('Name: '+name);
-    res.send('Title:' + title);
-});
+
 app.get('*',function(req,res){
     res.send('Bad Route');
 });
@@ -27,3 +30,5 @@ app.get('*',function(req,res){
 var server = app.listen(8080, function(){
     console.log('1003 App Started on 8080');
 });
+
+//2.7
